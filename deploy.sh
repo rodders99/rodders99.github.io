f5b8a71d4d1807a3bc3ca467626c43542cbc2690
Rodders <rod.slateruk@gmail.com>
2024-04-22 10:05:47 +0100

    Sorted malformed table

--- deploy.sh
+++ deploy.sh
@@ -6,4 +6,9 @@ DIR=www/   # the directory where your web site files should go
 
 hugo && rsync -avz --delete public/ ${USER}@${HOST}:~/${DIR} # this will delete everything on the server that's not in the local public folder 
 
+git add .
+read -p "Input commit message : " message
+git commit -m "$1"
+git push github
+
 exit 0
